2a75edd0edc7daf21b28dfcc570ed8298e166ac6
Cypphi <altcypphi@gmail.com>
2025-08-05 10:12:08 +0200

    Push broken code :0

--- src/main/java/dev/cypphi/eef/EchestExporterFabric.java
+++ src/main/java/dev/cypphi/eef/EchestExporterFabric.java
@@ -6,11 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EchestExporterFabric implements ModInitializer {
-        public static final String MOD_ID = "echest-exporter-fabric";
-        public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String MOD_ID = "echest-exporter-fabric";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-        @Override
-        public void onInitialize() {
-                // Mod initialization left intentionally empty.
-        }
+	@Override
+	public void onInitialize() {}
 }
