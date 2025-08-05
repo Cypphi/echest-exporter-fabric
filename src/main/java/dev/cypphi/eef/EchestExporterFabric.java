091a120d36703ebe3a29947fd463e1b42ab348c6
Cypphi <altcypphi@gmail.com>
2025-08-05 13:32:27 +0200

    Cleanup

--- src/main/java/dev/cypphi/eef/EchestExporterFabric.java
+++ src/main/java/dev/cypphi/eef/EchestExporterFabric.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EchestExporterFabric implements ClientModInitializer {
-	public static final String MOD_ID = "echest-exporter-fabric";
+	public static final String MOD_ID = "EEF";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
