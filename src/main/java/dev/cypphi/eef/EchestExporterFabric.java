5bb7eda9c3e7e36d469335ab3696a96d5e0fc35a
Cypphi <altcypphi@gmail.com>
2025-08-05 10:52:22 +0200

    why

--- src/main/java/dev/cypphi/eef/EchestExporterFabric.java
+++ src/main/java/dev/cypphi/eef/EchestExporterFabric.java
@@ -1,16 +1,14 @@
 package dev.cypphi.eef;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EchestExporterFabric implements ModInitializer {
+public class EchestExporterFabric implements ClientModInitializer {
 	public static final String MOD_ID = "echest-exporter-fabric";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-	}
+	public void onInitializeClient() {}
 }
